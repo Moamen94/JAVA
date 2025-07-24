89e3db700ece7c69eb0fb7e25803c3baa66a5042
Moamen94 <moomen.khasawneh@gmail.com>
2025-07-24 15:58:55 +0300

    ةخةشى

---
+++ Pyramid Star Patterns .js
@@ -0,0 +1,16 @@
+function printPyramid() {
+    const y = 13; 
+    const x = 9;   
+    for (let i = 1; i <= x; i++) {
+      
+        console.log("*".repeat(i));
+    }
+
+
+    for (let i = x - 1; i >= 1; i--) {
+      
+        console.log("*".repeat(i));
+    }
+}
+
+printPyramid();
